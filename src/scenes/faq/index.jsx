adbe665f74ca50330012f133352af121f06da8a4
rafadegolin <rafaeldegolin26@hotmail.com>
2024-07-21 19:57:29 -0300

    commit inicial

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,68 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography colors={colors.greenAccent[500]} variant="h5">
+            An Important question 1
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography colors={colors.greenAccent[500]} variant="h5">
+            An Important question 2
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography colors={colors.greenAccent[500]} variant="h5">
+            An Important question 3
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
+
+export default FAQ;
